609d73e453046e211a37466f4d52dd7823b2ece4
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2020-05-12 22:08:03 +0900

    초기 커밋.

---
+++ startree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace startree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 8 - 1; j > i; j--)
+                    Console.Write(" ");
+                for (int j =0 ; j < 2 * i + 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
